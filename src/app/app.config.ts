e80702d8dc800af2b1123cb9ea173a134ff9c807
Siebe.Dom <siebe.dom@economie.fgov.be>
2023-11-11 11:07:52 +0100

    Locale nl-BE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,14 @@ import {ApplicationConfig, LOCALE_ID} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {registerLocaleData} from "@angular/common";
+
+import localeNl from "@angular/common/locales/nl";
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     {provide: LOCALE_ID, useValue: 'nl-BE' }]
 };
+
+registerLocaleData(localeNl, "nl");
