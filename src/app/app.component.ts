736ee02faa2a35f42ec14ef805dff6002d5922fb
siebe.dom <siebe.dom@gmail.com>
2024-09-28 07:51:10 +0000

    Fix search issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'locale-demo';
 
+  private meta = inject(Meta); 
+  private router = inject(Router);
+
   constructor(){
-    
+  }
+
+  ngOnInit() {
+    this.setCanonicalAndAlternateLinks();
+  }
+
+  setCanonicalAndAlternateLinks() {
+    const lang = this.router.url.split('/')[1];  // Get the language from the URL
+    let canonicalUrl = `https://www.siebetest.be/${lang}/`;
+
+    if (!['nl', 'fr', 'en'].includes(lang)) {
+      canonicalUrl = 'https://www.siebetest.be/nl/';
+    }
+
+    // Set canonical link
+    this.meta.updateTag({ rel: 'canonical', href: canonicalUrl });
+
+    // Set alternate links
+    this.meta.updateTag({ rel: 'alternate', hreflang: 'nl', href: 'https://www.siebetest.be/nl/' });
+    this.meta.updateTag({ rel: 'alternate', hreflang: 'fr', href: 'https://www.siebetest.be/fr/' });
+    this.meta.updateTag({ rel: 'alternate', hreflang: 'en', href: 'https://www.siebetest.be/en/' });
+    this.meta.updateTag({ rel: 'alternate', hreflang: 'x-default', href: 'https://www.siebetest.be/nl/' });
   }
 }
